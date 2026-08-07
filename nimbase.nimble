a9a7251631fca9a9905f1764621498f2da19c755
George Lemon <georgelemon@protonmail.com>
2026-08-07 21:31:44 +0300

    split pkg/clue - init nimbase
    Signed-off-by: George Lemon <georgelemon@protonmail.com>

---
+++ nimbase.nimble
@@ -0,0 +1,21 @@
+# Package
+
+version       = "0.1.0"
+author        = "OpenPeeps"
+description   = "Nim Codegen – OAPI 3.x, C wrappers, FFI bindings & native extensions"
+license       = "MIT"
+srcDir        = "src"
+binDir        = "bin"
+bin           = @["nimbase"]
+
+installDirs = @["nimbase"]
+
+
+# Dependencies
+
+requires "nim >= 2.2.10"
+requires "semver >= 1.2.3"
+requires "kapsis >= 0.3.4"
+requires "boogie >= 0.1.0"
+requires "openparser >= 0.1.4"
+requires "sweetsyntax >= 0.1.0"
---
+++ src/nimbase.nim
@@ -0,0 +1,42 @@
+# Nimbase - A CLI for generating C wrappers, OAPI 3.x HTTP clients
+# native extensions for other programming languages and more
+#
+# (c) 2026 George Lemon | LGPLv3 License
+#          Made by Humans from OpenPeeps
+#          https://github.com/nimbase/nimbase
+
+when isMainModule:
+  # Build the CLI with Kapsis
+  import pkg/kapsis
+  import ./nimbase/commands/[kits_cmd, oapi_cmd]
+
+  initKapsis do:
+    commands:
+      #
+      # Build native extensions for other languages
+      # from your Nim code
+      #
+      -- "Native Extensions"
+      extension path(module), ?string("--ext"):
+        ## Build a native extension for other languages from Nim code
+      
+      -- "Code generator"
+      openapi:
+        ## OpenAPI 3.x utilities
+        init:
+          ## Initialize a default clue.openapi.config.yaml file
+        gen path(spec), string("output"), ?string("--config"), ?bool("-y"):
+          ## Generate a new API client library from OpenAPI 3.x spec file
+        mock path(spec), ?string("--host"), ?string("--port"):
+          ## Spin up a local mock server from OpenAPI 3.x spec file
+
+      # -- "Bundlers"
+      #   ## Commands for bundling plugins for different package managers
+      #   npm path(module):
+      #     ## Bundle a JavaScript N-API addon for publishing on npm
+      #   pypi path(module):
+      #     ## Bundle a Python extension for publishing on PyPI
+      #   pie path(module):
+      #     ## Bundle a PHP extension for publishing on PIE (PHP Installer for Extensions)
+else:
+  error("Nothing to see here. Import submodules you need directly")
